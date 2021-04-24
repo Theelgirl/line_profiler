0d096bb4b48947a8fab376289cb71d01913d41cd
Theelgirl <43764914+Theelgirl@users.noreply.github.com>
2021-04-23 21:41:58 -0400

    fix erroneous variable name

--- kernprof.py
+++ kernprof.py
@@ -266,8 +266,8 @@ def main(args=None):
     # kernprof.py's.
     sys.path.insert(0, os.path.dirname(script_file))
 
-    if options.output_interval_threaded:
-        rt = RepeatedTimer(max(options.output_interval_threaded, 1), prof.dump_stats, options.outfile)
+    if options.output_interval:
+        rt = RepeatedTimer(max(options.output_interval, 1), prof.dump_stats, options.outfile)
     try:
         try:
             execfile_ = execfile
@@ -279,7 +279,7 @@ def main(args=None):
         except (KeyboardInterrupt, SystemExit):
             pass
     finally:
-        if options.output_interval_threaded:
+        if options.output_interval:
             rt.stop()
         prof.dump_stats(options.outfile)
         print('Wrote profile results to %s' % options.outfile)
